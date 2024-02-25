6da1152c6476bdef9905e97ba4ffce85cf694020
sbuDiction <sibusisonkosi0897@gmail.com>
2024-02-25 14:57:06 +0200

    added new api url

--- src/globals.js
+++ src/globals.js
@@ -1,2 +1,2 @@
-// export const apiBaseUrl = 'https://load-shedding-api.onrender.com';
-export const apiBaseUrl = 'http://localhost:5000';
+export const apiBaseUrl = 'https://load-shedding-api.onrender.com';
+// export const apiBaseUrl = 'http://localhost:5000';
